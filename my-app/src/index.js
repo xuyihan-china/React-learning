f7a3ef446f9800be0c022b5fd62679211590888a
xuyihan-china <848247256@qq.com>
2020-08-28 14:29:23 +0800

    02

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -24,20 +24,44 @@ import * as serviceWorker from './serviceWorker';
 //   ReactDOM.render(element,root)
 // }
 // setInterval(clock,1000)
-let root = document.querySelector('#root')
+// let root = document.querySelector('#root')
 
 
-function Clock(insertNum) {
+// function Clock(insertNum) {
+//   return (
+//     <div>
+
+//       <div>the time right now is {insertNum.date}</div>
+//     </div>
+//   )
+// }
+// function run() {
+//   ReactDOM.render(<Clock date={new Date().toString()} />, root)
+// }
+// setInterval(run, 1000)
+let crg = 'csg'
+let fsb = 'fjd'
+let element = {
+  backgroundColor:'black',
+  color:'red'
+}
+function Cat() {
   return (
     <div>
-
-      <div>the time right now is {insertNum.date}</div>
+      <span>showcase</span>
+      <h2 className={crg + ' red'}>name xyh</h2>
+      <div>{1 + 1}</div>
+      <div style = {element}>{fsb+crg}</div>
     </div>
   )
 }
-function run() {
-  ReactDOM.render(<Clock date={new Date().toString()} />, root)
-}
-setInterval(run, 1000)
+// function Dog() {
+//   return (
+//     <div>
+//       <span className={crg}>This should be red</span>
+//     </div>
+//   )
+// }
+ReactDOM.render(<Cat />, document.querySelector('#root'))
 
 serviceWorker.unregister();
