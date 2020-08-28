d6773f2ff243e1bceccb5e4602859e87de315f31
xuyihan-china <848247256@qq.com>
2020-08-28 10:00:01 +0800

    -

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,15 +1,43 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+//import App from './App';
 import * as serviceWorker from './serviceWorker';
+/* app 是普通的js对象 */
+// let hh= <h1>make the better
+// </h1>
+// ReactDOM.render(
+//     hh,
+//   document.getElementById('root')
+// );
+//实现时刻显示时间
 
-ReactDOM.render(
-    <App />,
-  document.getElementById('root')
-);
+// function clock(){
+//   let root = document.querySelector('#root')
+//   let time = new Date().toString() //当前时间
+//   let element = (
+//   <div>
+//     <h1>the time right now is {time}</h1>
+//     <div>canvas</div>
+//   </div>
+//   )
+//   ReactDOM.render(element,root)
+// }
+// setInterval(clock,1000)
+let root = document.querySelector('#root')
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
+
+function Clock(insertNum) {
+  return (
+    <div>
+
+      <div>the time right now is {insertNum.date}</div>
+    </div>
+  )
+}
+function run() {
+  ReactDOM.render(<Clock date={new Date().toString()} />, root)
+}
+setInterval(run, 1000)
+
 serviceWorker.unregister();
