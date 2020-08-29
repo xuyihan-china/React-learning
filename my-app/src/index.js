e4264f98944baac0ab91e9bfc7d56b168541e066
xuyihan-china <848247256@qq.com>
2020-08-29 21:39:13 +0800

    00

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -65,14 +65,112 @@ import * as serviceWorker from './serviceWorker';
 //ReactDOM.render(<Cat />, document.querySelector('#root'))
 
 //写一个类组件
-let weather = "xiayu"
-let judge = weather == "xiayu" ? "gohone" : "goback"
-class Dog extends React.Component{
-  render(){
-    return(
-      <div>hello world</div>
+
+// function Cat() {
+//   return (
+//     <div>
+//       <div>haode</div>
+//       <h2>woyaog</h2>
+//     </div>
+//   )
+// }
+
+// class Dog extends React.Component {
+//   render() {
+//     return (
+//       <div>
+//         hello world
+//         <Cat/>
+//       </div>
+//     )
+//   }
+// }
+// ReactDOM.render(<Dog />, document.querySelector('#root'))
+
+/* clock 继承  */
+
+//let time = new Date().toString() //当前时间
+// class Clock extends React.Component{
+//   constructor(props){
+//     super(props)
+//     this.state ={
+//       time: new Date().toString()
+//     }
+//   }
+//   render(){
+//     return(
+//       <div>
+//         <h2>{this.state.time}</h2>
+//       </div>
+//     )
+
+//   }
+//生命周期函数 在组件渲染完成的时候调用
+//   componentDidMount(){
+//     setInterval(()=>{
+//         this.setState({
+//           time:new Date().toString()
+//         })
+//     },1000)
+//   }
+// }
+//一种传递值的方式是在组件内部
+
+// class Tab extends React.Component{
+//   constructor(props){
+//     super(props)
+//     this.state={
+//       isActive:"",
+//       strClass:""
+//     }
+//   }
+//   clickEvent(){
+//     console.log('clickEvent')
+//   }
+//   render(){
+//     return(
+//       <div>
+//         <button onClick={this.clickEvent}>content1</button>
+//         <button>content2</button>
+//         <div className="content">
+//           <h1>content1</h1>
+//         </div>
+//         <div className="content">
+//           <h1>content2</h1>
+//         </div>
+//       </div>
+//     )
+//   }
+// }
+//  ReactDOM.render(<Tab/>,document.querySelector('#root'))
+
+class Like extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      tab1: "i like you",
+      tab2: "you like me"
+    }
+    this.cab = this.cab.bind(this)
+  }
+  cab(){
+    console.log('click')
+    const tab2 = this.state.tab2
+    this.setState({tab2})
+  }
+  render() {
+    const tab1 = this.state.tab1
+    const tab2 = this.state.tab2
+    return (
+      <div>
+        <div onClick={this.cab}>{tab1}</div>
+        <div >{tab2}</div>
+      </div>
     )
   }
+  
+  
 }
-ReactDOM.render(<Dog/>,document.querySelector('#root'))
+
+ReactDOM.render(<Like />, document.querySelector('#root'))
 serviceWorker.unregister();
